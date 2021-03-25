fd59314ebe3f2355fe86a3f2094539468cd6f0c6
asmita <asmitahajra@gmail.com>
2021-03-25 08:47:33 +0530

    First commit backend-service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,11 @@
 const axios = require('axios').default;
 
 const verifyToken = async (token) => {
+  // const response = await axios.post('http://auth-service:7000/validateToken', {}, {
+  //   headers:
+  //   { Authorization: `Bearer ${token}` },
+  // });
+  // return response.data;
   const response = await axios.post('http://auth-service:7000/validateToken', {}, {
     headers:
     { Authorization: `Bearer ${token}` },
